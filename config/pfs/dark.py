125f9ed2513c146354c966c1bcdb538811933c4c
Andreas Ritter <azuri.ritter@gmail.com>
2016-04-19 13:55:06 -0400

    reduceDark WORKS

--- config/pfs/dark.py
+++ config/pfs/dark.py
@@ -7,8 +7,10 @@ config.load(os.path.join(getPackageDir("obs_pfs"), "config", "pfs", "isr.py"))
 config.darkTime = None
 
 config.isr.doBias = True
+config.isr.doDark=False
+config.isr.doFlat=False
 config.repair.cosmicray.nCrPixelMax = 1000000
 config.repair.cosmicray.minSigma = 5.0
 config.repair.cosmicray.min_DN = 50.0
-
-config.combination.combine = lsst.afw.math.MEDIAN
+config.isr.doLinearize=False
+#config.combination.combine = lsst.afw.math.MEDIAN
