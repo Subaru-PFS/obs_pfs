842c5ff98f8b5b5372f6f224c84e2cc7471b6373
Andreas Ritter <azuri.ritter@gmail.com>
2016-06-27 20:13:55 -0400

    Create new detector geometry files, add createRefSpec config

--- config/pfs/dark.py
+++ config/pfs/dark.py
@@ -1,16 +1,16 @@
-import os.path
+#import os.path
 
-from lsst.utils import getPackageDir
+#from lsst.utils import getPackageDir
 
-config.load(os.path.join(getPackageDir("obs_pfs"), "config", "pfs", "isr.py"))
+#config.load(os.path.join(getPackageDir("obs_pfs"), "config", "pfs", "isr.py"))
 
 config.darkTime = None
 
 config.isr.doBias = True
-config.isr.doDark=False
-config.isr.doFlat=False
-config.repair.cosmicray.nCrPixelMax = 1000000
+config.isr.doDark = False
+config.isr.doFlat = False
+#config.isr.doLinearize = False
+
+config.repair.cosmicray.nCrPixelMax = 5000000
 config.repair.cosmicray.minSigma = 5.0
 config.repair.cosmicray.min_DN = 50.0
-config.isr.doLinearize=False
-#config.combination.combine = lsst.afw.math.MEDIAN
