4f7ffe36818092e2b80f68daf661d899071dcb0c
Andreas Ritter <azuri.ritter@gmail.com>
2016-08-03 14:43:36 -0400

    Add base=10 to int(visit), maxCCDs=4

--- python/lsst/obs/pfs/ingest.py
+++ python/lsst/obs/pfs/ingest.py
@@ -23,6 +23,7 @@ class PfsParseTask(ParseTask):
         maxSpectrograph = 4
         minArmNum = 1
         maxArmNum = 4
+        maxCCDs = 4
         arms = ['b', 'r', 'n', 'm']
         sites = '[JLXIASPF]'
         categories = '[ABCDS]'
@@ -33,29 +34,25 @@ class PfsParseTask(ParseTask):
             if not matches:
                 raise RuntimeError("Unable to interpret filename: %s" % filename)
         site, category, visit, spectrograph, armNum = matches.groups()
-        print 'site = <'+site+'>, category = <'+category+'>, visit = <'+visit+'>, spectrograph = <'+spectrograph+'>, armNum = <'+armNum+'>'
         if int(spectrograph) < minSpectrograph or int(spectrograph) > maxSpectrograph:
             message = 'spectrograph (=',spectrograph,') out of bounds'
             raise Exception(message)
         ccd = int(spectrograph)-1
         if int(armNum) == minArmNum:
             """do nothing"""
-            #print 'armNum = ',armNum,' == minArmNum = ',minArmNum
         elif (int(armNum) == minArmNum + 1) or (int(armNum) == minArmNum + 3):
-            #print 'armNum = ',armNum,' == minArmNum +1 = ',minArmNum+1,' or armNum = ',armNum,' == minArmNum +1 = ',minArmNum+3
-            ccd += 4
+            ccd += maxCCDs
         elif int(armNum) == minArmNum + 2:
-            #print 'armNum = ',armNum,' == minArmNum+2 = ',minArmNum+2
-            ccd += 8
+            ccd += 2*maxCCDs
         else:
             message = 'arm number (=',armNum,') out of bounds [',minArmNum,'...',maxArmNum,']'
             raise Exception(message)
 
-        #print 'ccd = ',ccd
         filter = arms[ int(armNum) - minArmNum ]
         arm = filter
+        print 'site = <'+site+'>, category = <'+category+'>, visit = <'+visit+'>, spectrograph = <'+spectrograph+'>, armNum = <'+armNum+'>, filter = <'+filter+'>, ccd = ',ccd
 
-        info = dict(site=site, category=category, visit=int(visit), filter=filter, arm=arm, spectrograph=int(spectrograph), ccd=int(ccd))
+        info = dict(site=site, category=category, visit=int(visit, base=10), filter=filter, arm=arm, spectrograph=int(spectrograph), ccd=ccd)
         if os.path.exists(filename):
             header = afwImage.readMetadata(filename)
             info = self.getInfoFromMetadata(header, info=info)
