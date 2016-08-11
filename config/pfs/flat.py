f588cf8758377fa92e74aed1c47bb77a20710d69
Andreas Ritter <azuri.ritter@gmail.com>
2016-08-10 21:08:15 -0400

    Change some parameter overrides

--- config/pfs/flat.py
+++ config/pfs/flat.py
@@ -1,17 +1,17 @@
-#import os.path
+import os.path
 
-#from lsst.utils import getPackageDir
+from lsst.utils import getPackageDir
 
-#config.load(os.path.join(getPackageDir("obs_pfs"), "config", "pfs", "isr.py"))
+config.load(os.path.join(getPackageDir("obs_pfs"), "config", "pfs", "isr.py"))
 
 #from lsst.pipe.drivers.constructCalibs import CalibTask
 ##config.combination.retarget(CalibTask)
 #from lsst.obs.pfs.detrends import PfsFlatCombineTask
 #config.combination.retarget(PfsFlatCombineTask)
-##config.combination.load(os.path.join(os.environ['OBS__DIR'], 'config', 'pfs', 'flat.py'))
+##config.combination.load(os.path.join(os.environ['OBS_PFS_DIR'], 'config', 'pfs', 'flat.py'))
 
 config.isr.doBias = True
 config.isr.doDark = True
 config.isr.doFlat = False
 config.isr.doFringe = False
-#config.isr.doLinearize = False
+config.isr.doLinearize = False
