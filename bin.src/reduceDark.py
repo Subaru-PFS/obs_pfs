8d024751b618136bd7b8a96e9a7d56a41ddc8751
Andreas Ritter <azuri.ritter@gmail.com>
2016-08-11 15:25:47 -0400

    Remove obsolete reduce... as they are now provided by pipe_drivers

--- bin.src/reduceDark.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-#/Users/azuri/stella-git/drp_stella/bin.src/reduceDark.py '/Volumes/My Passport/data/spectra/pfs/PFS' --calib '/Volumes/My Passport/data/spectra/pfs/PFS/CALIB' --detrendId calibVersion='dark' site='S' category='A' --id field='DARK' dateObs='2015-12-22' spectrograph=2 site='S' category='A' filter='PFS-M' visit=7291..7294 --loglevel info --clobber-config  -c isr.doBias=True --do-exec
-from lsst.pipe.drivers.constructCalibs import DarkTask
-DarkTask.parseAndSubmit()
