8c1b60c017f33ebd9e67ed073df77f233912286a
Andreas Ritter <azuri.ritter@gmail.com>
2016-08-18 03:22:06 -0400

    Change print commands to logdebug messages

--- python/lsst/obs/pfs/ingest.py
+++ python/lsst/obs/pfs/ingest.py
@@ -18,7 +18,7 @@ class PfsParseTask(ParseTask):
         @param filename    Name of file to inspect
         @return File properties; list of file properties for each extension
         """
-        #print 'interpreting filename <'+filename+'>'
+        self.log.debug('interpreting filename <%s>' % filename)
         minSpectrograph = 1
         maxSpectrograph = 4
         minArmNum = 1
@@ -52,7 +52,7 @@ class PfsParseTask(ParseTask):
 
         filter = arms[ int(armNum) - minArmNum ]
         arm = filter
-        print 'site = <'+site+'>, category = <'+category+'>, visit = <'+visit+'>, spectrograph = <'+spectrograph+'>, armNum = <'+armNum+'>, filter = <'+filter+'>, ccd = ',ccd
+        self.log.debug('site = <%s>, category = <%s>, visit = <%s>, spectrograph = <%s>, armNum = <%s>, filter = <%s>, ccd = %d' % (site, category, visit, spectrograph, armNum, filter, ccd))
 
         info = dict(site=site, category=category, visit=int(visit, base=10), filter=filter, arm=arm, spectrograph=int(spectrograph), ccd=ccd)
         if os.path.exists(filename):
@@ -69,5 +69,5 @@ class PfsParseTask(ParseTask):
         field = md.get("IMAGETYP").strip()
         if field in ("#", ""):
             field = "UNKNOWN"
-        self.log.info('PfsParseTask.translate_field: field = %s' % field)
+        self.log.debug('PfsParseTask.translate_field: field = %s' % field)
         return re.sub(r'\W', '_', field).upper()
