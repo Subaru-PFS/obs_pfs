70490283231233dbe64e8f897bc7826f0ef3636d
Andreas Ritter <azuri.ritter@gmail.com>
2016-10-20 23:57:46 -0400

    Add ConstructArcTask

---
+++ config/constructArc.py
@@ -0,0 +1,8 @@
+config.isr.doBias = True
+config.isr.doDark = False
+config.isr.doFlat = False
+config.isr.doLinearize = False
+
+config.repair.cosmicray.nCrPixelMax = 50000
+config.repair.cosmicray.minSigma = 5.0
+config.repair.cosmicray.min_DN = 50.0
