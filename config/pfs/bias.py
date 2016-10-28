65c3db9cd0eee97a2f72203023aa2aa0c16cf293
Andreas Ritter <azuri.ritter@gmail.com>
2016-10-27 23:21:39 -0400

    Update and clean up config files

--- config/pfs/bias.py
+++ config/pfs/bias.py
@@ -4,7 +4,7 @@ from lsst.utils import getPackageDir
 
 config.load(os.path.join(getPackageDir("obs_pfs"), "config", "pfs", "isr.py"))
 
-config.isr.doBias = True
+config.isr.doBias = False
 config.isr.doDark = False
 config.isr.doFlat = False
 config.isr.doFringe = False
--- config/pfs/constructFiberFlat.py
+++ config/pfs/constructFiberFlat.py
@@ -1,11 +1,8 @@
-config.darkTime = None
-
 config.isr.doBias = True
-config.isr.doDark = False
+config.isr.doDark = True
 config.isr.doFlat = False
-#config.isr.doLinearize = False
+config.isr.doLinearize = False
 
 config.repair.cosmicray.nCrPixelMax = 5000000
 config.repair.cosmicray.minSigma = 5.0
 config.repair.cosmicray.min_DN = 50.0
-
