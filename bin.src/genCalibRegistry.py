a6930c8b9e4c535d2b46bee0208e8d4bf8680984
Paul Price <price@astro.princeton.edu>
2016-11-16 20:15:44 -0500

    genCalibRegistry: modernise
    Put functionality into a function.
    Remove postgres support (unused).
    ArugmentParser instead of OptionParser.
    Remove camera argument (only works for PFS).

--- bin.src/genCalibRegistry.py
+++ bin.src/genCalibRegistry.py
@@ -1,148 +1,98 @@
 #!/usr/bin/env python
-#/Users/azuri/stella-git/obs_subaru/bin.src/genCalibRegistry.py --root='/Volumes/My Passport/data/spectra/pfs/PFS/CALIB' --camera PFS --validity 180
 import glob
 import os
 import re
-try:
-    import sqlite3 as sqlite
-except ImportError:
-    import sqlite
+import sqlite3 as sqlite
 import sys
 import datetime
 import collections
-import optparse
+import argparse
 
-try:
-    import psycopg2 as pgsql
-    havePgSql = True
-except ImportError:
-    try:
-        from pg8000 import DBAPI as pgsql
-        havePgSql = True
-    except ImportError:
-        havePgSql = False
-if havePgSql:
-    from lsst.daf.butlerUtils import PgSqlConfig
+def main(root, validityDays):
 
-# Needs optparse w/ --create, etc. CPL
-parser = optparse.OptionParser()
-parser.add_option("--create", dest="create", default=False, action="store_true",
-                  help="Create new registry (clobber old)?")
-parser.add_option("--root", dest="root", default=".", help="Root directory")
-parser.add_option("--camera", dest="camera", default="hsc", help="Camera name: HSC|SC|PFS")
-parser.add_option("--validity", type="int", dest="validity", default=30, help="Calibration validity (days)")
-opts, args = parser.parse_args()
-
-if len(args) > 0 or len(sys.argv) == 1:
-    print "Unrecognised arguments:", sys.argv[1:]
-    parser.print_help()
-    sys.exit(1)
-
-if opts.camera.lower() not in ("suprime-cam", "suprimecam", "sc", "hsc", "hscsim", "pfs"):
-    raise RuntimeError("Camera not recognised: %s" % camera)
-
-if os.path.exists(os.path.join(opts.root, 'calibRegistry_pgsql.py')) and havePgSql:
-    isSqlite = False
-else:
-    isSqlite = True
-
-if isSqlite:
-    registry = os.path.join(opts.root, "calibRegistry.sqlite3")
+    registry = os.path.join(root, "calibRegistry.sqlite3")
 
     if os.path.exists(registry):
         os.unlink(registry)
     conn = sqlite.connect(registry)
-else:
-    pgsqlConf = PgSqlConfig()
-    pgsqlConf.load(os.path.join(opts.root, 'calibRegistry_pgsql.py'))
-    conn = pgsql.connect(host=pgsqlConf.host, port=pgsqlConf.port,
-                         user=pgsqlConf.user, password=pgsqlConf.password,
-                         database=pgsqlConf.db)
-    cur = conn.cursor()
 
-Row = collections.namedtuple("Row", ["calibDate", "calibVersion", "spectrograph", "arm", "ccd"])
+    Row = collections.namedtuple("Row", ["calibDate", "calibVersion", "spectrograph", "arm", "ccd"])
 
-for calib in ('bias', 'dark', 'flat', 'fiberFlat', 'imageFlat', 'fiberTrace'):
-    if isSqlite:
+    for calib in ('bias', 'dark', 'flat', 'fiberFlat', 'imageFlat', 'fiberTrace'):
         cmd = "create table " + calib.lower() + " (id integer primary key autoincrement"
         cmd += ", validStart text, validEnd text"
         cmd += ", calibDate text, filter text, calibVersion text, spectrograph int, arm text, ccd int"
         cmd += ")"
         conn.execute(cmd)
-    else:
-        cmd = "DROP TABLE IF EXISTS " + calib.lower()
-        cur.execute(cmd)
-        cmd = "create table " + calib.lower() + " (id SERIAL NOT NULL PRIMARY KEY"
-        cmd += ", validStart VARCHAR(10), validEnd VARCHAR(10)"
-        cmd += ", calibDate VARCHAR(10), filter VARCHAR(5), calibVersion VARCHAR(5), spectrogaph INT, arm VARCHAR(1), ccd INT"
-        cmd += ")"
-        cur.execute(cmd)
-    conn.commit()
+        conn.commit()
 
-    rowsPerFilter = dict()
+        rowsPerFilter = dict()
 
-    checkFits = os.path.join(opts.root, calib.upper(), "*", "*", "*.fits")
+        checkFits = os.path.join(root, calib.upper(), "*", "*", "*.fits")
 
-    for fits in glob.glob(checkFits):
-        m = re.search(r'\w+/([\w-]+)/([\w-]+)/pfs\w+-(\d{4})-(\d{2})-(\d{2})-0-(\d)(\w).fits', fits)#pfsBias-007251-2m.fits
-        if not m:
-            m = re.search(r'\w+/([\w-]+)/(\d{4})-(\d{2})-(\d{2})/pfs(\w+)-(\d{6})-(\d)(\w).fits', fits)#pfsBias-007251-2m.fits
+        for fits in glob.glob(checkFits):
+            m = re.search(r'\w+/([\w-]+)/([\w-]+)/pfs\w+-(\d{4})-(\d{2})-(\d{2})-0-(\d)(\w).fits', fits)
             if not m:
-                print >>sys.stderr, "Warning: Unrecognized file name:", fits
-                raise Exception("Unrecognized file name")
+                m = re.search(r'\w+/([\w-]+)/(\d{4})-(\d{2})-(\d{2})/pfs(\w+)-(\d{6})-(\d)(\w).fits', fits)
+                if not m:
+                    print >>sys.stderr, "Warning: Unrecognized file name:", fits
+                    raise Exception("Unrecognized file name")
+                else:
+                    filterName, year, month, day, version, visit, spectrograph, arm = m.groups()
+                    version = version[0].lower() + version[1:]
             else:
-                filterName, year, month, day, version, visit, spectrograph, arm = m.groups()
-                version = version[0].lower() + version[1:]
-        else:
-            filterName, version, year, month, day, spectrograph, arm = m.groups()
+                filterName, version, year, month, day, spectrograph, arm = m.groups()
 
-        print "Registering:", fits
-        date = datetime.date(int(year), int(month), int(day))
-        ccd = int(spectrograph) - 1
-        if arm in ("m"):
-            ccd = ccd + 4
-        elif arm in ("r"):
-            ccd += 4
-        elif arm in ("n"):
-            ccd += 8
+            print "Registering:", fits
+            date = datetime.date(int(year), int(month), int(day))
+            ccd = int(spectrograph) - 1
+            if arm in ("m"):
+                ccd = ccd + 4
+            elif arm in ("r"):
+                ccd += 4
+            elif arm in ("n"):
+                ccd += 8
 
-        if filterName not in rowsPerFilter:
-            rowsPerFilter[filterName] = list()
-        rowsPerFilter[filterName].append(Row(date, version, spectrograph, arm, ccd))
+            if filterName not in rowsPerFilter:
+                rowsPerFilter[filterName] = list()
+            rowsPerFilter[filterName].append(Row(date, version, spectrograph, arm, ccd))
 
-    # Fix up the validStart,validEnd so there are no overlaps
-    for filterName, rows in rowsPerFilter.items():
-        rows.sort(key=lambda row: row.calibDate)
-        validity = datetime.timedelta(opts.validity)
-        valids = collections.OrderedDict([(row.calibDate, [row.calibDate - validity,
-                                                           row.calibDate + validity]) for row in rows])
-        dates = valids.keys()
-        numDates = len(dates)
-        midpoints = [ t1 + (t2 - t1)//2 for t1, t2 in zip(dates[:numDates-1], dates[1:]) ]
-        for i, (date, midpoint) in enumerate(zip(dates[:numDates-1], midpoints)):
-            if valids[date][1] > midpoint:
-                nextDate = dates[i+1]
-                #print "Adjusting: %d %s --> %s : %s vs %s" % (i, date, nextDate, valids[date][1], midpoint)
-                valids[nextDate][0] = midpoint
-                valids[date][1] = midpoint
-        del midpoints
-        del dates
+        # Fix up the validStart,validEnd so there are no overlaps
+        for filterName, rows in rowsPerFilter.items():
+            rows.sort(key=lambda row: row.calibDate)
+            validity = datetime.timedelta(validityDays)
+            valids = collections.OrderedDict([(row.calibDate, [row.calibDate - validity,
+                                                               row.calibDate + validity]) for row in rows])
+            dates = valids.keys()
+            numDates = len(dates)
+            midpoints = [ t1 + (t2 - t1)//2 for t1, t2 in zip(dates[:numDates-1], dates[1:]) ]
+            for i, (date, midpoint) in enumerate(zip(dates[:numDates-1], midpoints)):
+                if valids[date][1] > midpoint:
+                    nextDate = dates[i+1]
+                    valids[nextDate][0] = midpoint
+                    valids[date][1] = midpoint
+            del midpoints
+            del dates
 
-        for row in rows:
-            calibDate = row.calibDate.isoformat()
-            validStart = valids[row.calibDate][0].isoformat()
-            validEnd = valids[row.calibDate][1].isoformat()
+            for row in rows:
+                calibDate = row.calibDate.isoformat()
+                validStart = valids[row.calibDate][0].isoformat()
+                validEnd = valids[row.calibDate][1].isoformat()
 
-            # print "%f --> %f %f" % (calibDate, validStart, validEnd)
-
-            if isSqlite:
                 conn.execute("INSERT INTO " + calib.lower() + " VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?)",
-                            (validStart, validEnd, calibDate, filterName, row.calibVersion, row.spectrograph, row.arm, row.ccd))
-            else:
-                cur.execute("INSERT INTO " + calib.lower() + " (validStart, validEnd, calibDate, filter, calibVersion, spectrograph, arm, ccd) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
-                            (validStart, validEnd, calibDate, filterName, row.calibVersion, row.spectrograph, row.arm, row.ccd))
+                             (validStart, validEnd, calibDate, filterName, row.calibVersion, row.spectrograph,
+                              row.arm, row.ccd))
 
-conn.commit()
-if not isSqlite:
-    cur.close()
-conn.close()
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--root", default=".", required=True, help="Root directory")
+    parser.add_argument("--validity", type=int, dest="validity", default=30,
+                        help="Calibration validity (days)")
+    args = parser.parse_args()
+
+    main(args.root, args.validity)
+
