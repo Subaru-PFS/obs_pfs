544a2a55aecb1ab5a306d18f1c73548d0e8cf248
Andreas Ritter <azuri.ritter@gmail.com>
2017-02-09 18:29:29 -0500

    ingest.py: get CCD ID from mapper instead of assigning it manually
    The CCD number/ID [0..11] was manually assigned in
    ingest.py and had the range[1..12] before just like PfsMapper.
    To remove the code duplication we now call
    PfsMapper.getDetectorId(dataId) to get the CCD number/ID.

--- python/lsst/obs/pfs/ingest.py
+++ python/lsst/obs/pfs/ingest.py
@@ -3,6 +3,8 @@ import re
 import os
 from lsst.pipe.tasks.ingest import ParseTask, ParseConfig
 import lsst.afw.image as afwImage
+from lsst.obs.pfs.pfsMapper import PfsMapper
+import lsst.utils
 
 class PfsParseConfig(ParseConfig):
     def setDefaults(self):
@@ -23,7 +25,6 @@ class PfsParseTask(ParseTask):
         maxSpectrograph = 4
         minArmNum = 1
         maxArmNum = 4
-        maxCCDs = 4
         arms = ['b', 'r', 'n', 'm']
         sites = '[JLXIASPF]'
         categories = '[ABCDS]'
@@ -34,25 +35,28 @@ class PfsParseTask(ParseTask):
             if not matches:
                 raise RuntimeError("Unable to interpret filename: %s" % filename)
         site, category, visit, spectrograph, armNum = matches.groups()
+
+        # spectrograph '2' was called '9' at JHU, at least in the early days, so if we find
+        # a spectrograph '9' we re-assign its number to '2'
         if int(spectrograph) == 9:
             spectrograph = '2'
+
         if int(spectrograph) < minSpectrograph or int(spectrograph) > maxSpectrograph:
-            message = 'spectrograph (=',spectrograph,') out of bounds'
-            raise Exception(message)
-        ccd = int(spectrograph)-1
-        if int(armNum) == minArmNum:
-            """do nothing"""
-        elif (int(armNum) == minArmNum + 1) or (int(armNum) == minArmNum + 3):
-            ccd += maxCCDs
-        elif int(armNum) == minArmNum + 2:
-            ccd += 2*maxCCDs
-        else:
-            message = 'arm number (=',armNum,') out of bounds [',minArmNum,'...',maxArmNum,']'
-            raise Exception(message)
+            raise Exception('spectrograph (=$s) out of bounds' % spectrograph)
+
+        if int(armNum) < minArmNum or int(armNum) > maxArmNum:
+            raise Exception('armNum (=$s) out of bounds' % armNum)
 
         filter = arms[ int(armNum) - minArmNum ]
         arm = filter
-        self.log.debug('site = <%s>, category = <%s>, visit = <%s>, spectrograph = <%s>, armNum = <%s>, filter = <%s>, ccd = %d' % (site, category, visit, spectrograph, armNum, filter, ccd))
+
+        dataId = {'spectrograph': int(spectrograph), 'arm': arm}
+        mapper = PfsMapper(root=lsst.utils.getPackageDir('drp_stella'))
+        ccd = mapper.getDetectorId(dataId)
+        self.log.info(
+            'site = <%s>, category = <%s>, visit = <%s>, spectrograph = <%s>, armNum = <%s>, filter = <%s>, ccd = <%d>'
+            % (site,category,visit,spectrograph,armNum,filter,ccd)
+        )
 
         info = dict(site=site, category=category, visit=int(visit, base=10), filter=filter, arm=arm, spectrograph=int(spectrograph), ccd=ccd)
         if os.path.exists(filename):
