f23ce2501b2cee0ce4cf26300151133598384cdf
Andreas Ritter <azuri.ritter@gmail.com>
2017-02-09 18:45:32 -0500

    tests/Mapper.py: Add tests for _extractDetectorName

--- tests/Mapper.py
+++ tests/Mapper.py
@@ -27,13 +27,13 @@ class MapperTestCase(tests.TestCase):
             shutil.rmtree(self.butlerDir)
         os.makedirs(self.butlerDir)
         os.makedirs(os.path.join(self.butlerDir,'CALIB'))
-        
+
         """Write mapper"""
         mapper = "lsst.obs.pfs.PfsMapper"
         text_file = open(os.path.join(self.butlerDir,"_mapper"), "w")
         text_file.write(mapper)
         text_file.close()
-        
+
         self.rawFiles = os.path.join(drpStellaDataDir,"tests/data/raw/*.fits")
         self.regFile = os.path.join(self.butlerDir,'registry.sqlite3')
         self.dataId = {'visit': '4', 'ccd': 1}
@@ -72,6 +72,48 @@ class MapperTestCase(tests.TestCase):
                 self.assertTrue(info[0]['arm'] == arms[armNum-1])
                 self.assertTrue(info[0]['filter'] == arms[armNum-1])
                 self.assertTrue(info[0]['ccd'] == ccd)
+
+    def testDetectorId(self):
+        butler = dafPersist.Butler(self.butlerDir)
+        mapper = butler.getMapperClass(self.butlerDir)(root=self.butlerDir)
+        camera = mapper.camera
+        blues=[0,3,6,9]
+        reds=[1,4,7,10]
+        nirs=[2,5,8,11]
+        ones=[0,1,2]
+        twos=[3,4,5]
+        threes=[6,7,8]
+        fours=[9,10,11]
+        for armNum in np.arange(self.minArmNum, self.maxArmNum + 1, 1):
+            for spectrograph in np.arange(self.minSpectrographNum, self.maxSpectrographNum + 1, 1):
+                dataId = {'visit': self.visit, 'spectrograph': spectrograph, 'arm':self.arms[armNum-1]}
+                detectorId = mapper._extractDetectorId(dataId)
+                if self.arms[armNum-1] in ['b']:
+                    self.assertTrue(detectorId in blues)
+                elif self.arms[armNum-1] in ['r', 'm']:
+                    self.assertTrue(detectorId in reds)
+                elif self.arms[armNum-1] in ['n']:
+                    self.assertTrue(detectorId in nirs)
+                else:
+                    self.assertTrue(False)
+
+                if spectrograph in [1]:
+                    self.assertTrue(detectorId in ones)
+                elif spectrograph in [2]:
+                    self.assertTrue(detectorId in twos)
+                elif spectrograph in [3]:
+                    self.assertTrue(detectorId in threes)
+                elif spectrograph in [4]:
+                    self.assertTrue(detectorId in fours)
+                else:
+                    self.assertTrue(False)
+
+                name = mapper._extractDetectorName(dataId)
+                for ccd in camera:
+                    ccdNum = ccd.getId()
+                    ccdName = ccd.getName()
+                    if ccdName in [name]:
+                        self.assertEqual(ccdNum, detectorId)
 
 def suite():
     """Returns a suite containing all the test cases in this module."""
