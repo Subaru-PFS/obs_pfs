48b223ad4013d6471eecfe57d9a88d37b3455ea3
Robert Lupton the Good <rhl@astro.princeton.edu>
2017-07-27 11:54:35 -0400

    Don't hard-code filenames that are in the datamodel.

--- tests/PfsFiberTrace.py
+++ tests/PfsFiberTrace.py
@@ -50,7 +50,6 @@ class PfsFiberTraceTestCase(tests.TestCase):
         # This particular flatfile has 12 FiberTraces scattered over the whole CCD
         # If in the future the test data change we need to change these numbers
         self.nFiberTraces = 11
-        self.fileNameFormat = "pfsFiberTrace-%10s-0-%1d%1s.fits"
 
     def tearDown(self):
         del self.flat
@@ -73,12 +72,11 @@ class PfsFiberTraceTestCase(tests.TestCase):
         #Check that we can write a PfsFiberTrace from the fiberTraceSet
         dataId = dict(calibDate=self.dataId['dateObs'], spectrograph=self.dataId['spectrograph'], arm=self.dataId['arm'])
         pfsFiberTrace = createPfsFiberTrace(dataId, fiberTraceSet, self.flat.getHeight())
-        fileName = os.path.join('.',PfsFiberTrace.fileNameFormat % (self.dataId['dateObs'], self.dataId['spectrograph'], self.dataId['arm']))
-        pfsFiberTraceIO = PfsFiberTraceIO(pfsFiberTrace)
-        pfsFiberTraceIO.writeFits(fileName)
 
+        pfsFiberTrace.write()
         #Check that we can read a PfsFiberTrace back in
-        pfsFiberTraceNew = PfsFiberTrace(self.dataId['dateObs'], self.dataId['spectrograph'], self.dataId['arm'])
+        pfsFiberTraceNew = PfsFiberTrace(self.dataId['dateObs'],
+                                         self.dataId['spectrograph'], self.dataId['arm'])
         pfsFiberTraceNew.read()
 
         self.assertEqual(pfsFiberTrace.obsDate, pfsFiberTraceNew.obsDate)
