b4425bae68721e6ac9171d0058344bb143709242
Paul Price <price@astro.princeton.edu>
2018-10-03 16:57:14 -0400

    strip dependency on obs_subaru
    It was only required for using the CrosstalkTask, but that feature
    was disabled anyway.

--- config/isr.py
+++ config/isr.py
@@ -1,11 +1,6 @@
 # PFS configuration for lsst.ip.isr.IsrTask
 # 'config' is a lsst.ip.isr.IsrConfig
 
-if False:  # Disabled until we have crosstalk coefficients
-    from lsst.obs.subaru.crosstalk import CrosstalkTask
-    config.crosstalk.retarget(CrosstalkTask)
-    config.doCrosstalk = True
-
 config.expectWcs = False            # our spectrographs don't write a WCS to the header
 config.doLinearize = False
 config.doFringe = False
