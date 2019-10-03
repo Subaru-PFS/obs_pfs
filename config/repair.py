dcd59bd55cf4d2720e13ba4ca55d880ace638d07
Paul Price <price@astro.princeton.edu>
2019-10-03 13:17:54 -0400

    config: modify cosmicray cond3 values
    Condition 3 in the cosmic ray identification code is the test for
    sharpness.
    
    In simulated images with full fiber density, many of the traces
    are being identified as CRs. This appears to be because cond3_fac
    is set too low. The limit on sharpness is set using a PSF realised
    at the center of a pixel, but PSFs that are not centered on the
    pixel can have even sharper edges. Increasing cond3_fac allows for
    this extra sharpness.
    
    The condition 3 parameter values used for HSC (which are the defaults)
    do a much better job of not being overwhelmed by the sharp edges of the
    fiber traces.

--- config/repair.py
+++ config/repair.py
@@ -4,8 +4,7 @@
 # This CR tuning comes from looking at simulated r1 images w/o scattering.
 #
 config.interp.modelPsf.defaultFwhm = 1.75
-config.cosmicray.cond3_fac = 4
-config.cosmicray.cond3_fac2 = 1
+config.cosmicray.cond3_fac = 30
 config.cosmicray.nCrPixelMax = 5000000
 config.cosmicray.minSigma = 10.0
 config.cosmicray.min_DN = 500.0
