9d41a4be63dfad86043cc2086143f88d62471501
Paul Price <price@astro.princeton.edu>
2019-10-23 18:25:43 -0400

    config: disable single-image CR in flat construction
    It takes too long, and we get rid of the CRs through
    combining multiple images with rejection.

--- config/fiberFlat.py
+++ config/fiberFlat.py
@@ -7,3 +7,6 @@ config.load(os.path.join(getPackageDir("obs_pfs"), "config", "fiberTrace.py"))
 # This is specifically for flat creation, where the quartz spectra are narrowly spaced.
 config.trace.function.xLow = -3
 config.trace.function.xHigh = 3
+
+# Disable cosmic-ray detection: it takes too long, and we weed them out through combining with rejection
+config.doRepair = False
