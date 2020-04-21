fc3e089e177f18549f221404c0bda9a2214c2c27
Paul Price <price@astro.princeton.edu>
2020-04-21 13:59:38 -0400

    PfsMapper: fix lookup of pfsMerged
    butler.get('pfsMerged', visit=12345) was failing, because it
    was trying to look up additional keys that aren't defined for
    a merged product.

--- python/lsst/obs/pfs/pfsMapper.py
+++ python/lsst/obs/pfs/pfsMapper.py
@@ -95,7 +95,7 @@ class PfsMapper(CameraMapper):
                 'pfsDesignId': int,
                 'slitOffset': float,
                 }
-        for name in ("raw", "pfsArm", "wlFitData", "pfsMerged"):
+        for name in ("raw", "pfsArm", "wlFitData"):
             self.mappings[name].keyDict.update(keys)
 
         # The order of these defineFilter commands matters as their IDs are used to generate at least some
