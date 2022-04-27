ad5e9f183fd6167f8791a6a394c1e734b431461d
Paul Price <price@astro.princeton.edu>
2022-04-27 17:36:04 -0400

    translator: update in preparation for Gen3
    The LSST Gen3 middleware makes heavy use of the PfsTranslator for ingest
    of raw files. Added properties that were missing, including new "extension"
    properties that are unique to PFS. Updated old properties that were based
    on old readout electronics.

--- python/lsst/obs/pfs/translator.py
+++ python/lsst/obs/pfs/translator.py
@@ -1,15 +1,15 @@
 import os
-import re
-from types import SimpleNamespace
 
 import numpy as np
 import astropy.units as u
-from astropy.coordinates import SkyCoord
+from astropy.coordinates import SkyCoord, Angle, AltAz
 
-from astro_metadata_translator import SubaruTranslator, cache_translation
+from astro_metadata_translator import SubaruTranslator, cache_translation, PropertyDefinition
 from astro_metadata_translator.translators.helpers import altaz_from_degree_headers
 
 from lsst.utils import getPackageDir
+
+from .utils import getLamps
 
 __all__ = ["PfsTranslator"]
 
@@ -29,21 +29,28 @@ class PfsTranslator(SubaruTranslator):
     default_resource_root = os.path.join(getPackageDir("obs_pfs"), "corrections")
     """Default resource path root to use to locate header correction files."""
 
-    _const_map = {"boresight_rotation_angle": "unknown",
-                  "boresight_rotation_coord": "unknown",
+    _const_map = {"boresight_rotation_angle": Angle(0.0*u.deg),
+                  "boresight_rotation_coord": "sky",
                   "telescope": "Subaru",
+                  "observation_type": "science",
                   }
     """Constant mappings"""
 
     _trivial_map = {
         # Existing
-        "dark_time": ("DARKTIME", dict(unit=u.s)),
-        "detector_group": "SPECNUM",  # Spectrograph
-        "detector_name": "ARM",
+        "exposure_id": "W_VISIT",
+        "observation_id": "W_VISIT",
+        "visit_id": "W_VISIT",
+        "detector_group": "W_SPMOD",
         "exposure_time": ("EXPTIME", dict(unit=u.s)),
+        "detector_serial": "DETECTOR",
         "object": "IMAGETYP",  # XXX to be updated; hopefully to OBJECT
-        "observation_type": "IMAGETYP",
-        "physical_filter": "ARM",
+        "ext_spectrograph": "W_SPMOD",
+        "ext_arm": "W_ARM",
+        "ext_pfs_design_id": "W_PFDSGN",
+        "ext_dither": "W_ENFCAZ",
+        "ext_shift": "W_ENFCAY",
+        "ext_focus": "W_ENFCAX",
         # Guesses for what we will use in the future
         "boresight_airmass": ("AIRMASS", dict(default=np.nan)),
         "pressure": ("OUT-PRS", dict(unit=u.hPa, default=np.nan)),
@@ -53,15 +60,15 @@ class PfsTranslator(SubaruTranslator):
     }
     """One-to-one mappings"""
 
-    _armToIndex = {"blue": 1,
-                   "red": 2,
-                   "nir": 3,
-                   "medium": 4,
-                   }
-
-    def __init__(self, header, filename=None):
-        super().__init__(header, filename=filename)
-        self._parsedData = self.parseFilename() if filename is not None else None
+    extensions = dict(
+        pfs_design_id=PropertyDefinition("Top-end configuration ID", "int", int),
+        spectrograph=PropertyDefinition("Spectrograph module [1-4]", "int", int),
+        arm=PropertyDefinition("Arm of spectrograph [brnm]", "str", str),
+        dither=PropertyDefinition("Slit offset in spatial dimension", "float", float),
+        shift=PropertyDefinition("Slit offset in spectral dimension", "float", float),
+        focus=PropertyDefinition("Focus offset", "float", float),
+        lamps=PropertyDefinition("Lamps that are in use", "str", str),
+    )
 
     @classmethod
     def can_translate(cls, header, filename=None):
@@ -81,40 +88,13 @@ class PfsTranslator(SubaruTranslator):
             `True` if the header is recognized by this class. `False`
             otherwise.
         """
-        if "INSTRUME" in header:  # Not in header as of August 2019
-            return header["INSTRUME"] == "PFS"
-        if "HIERARCH revision.FEE" in header and header["HIERARCH revision.FEE"].startswith("PFS"):
-            return True
-        return False
-
-    def parseFilename(self):
-        if self.filename is None:
-            raise ValueError("No filename to parse")
-        sites = '[JLXIASPF]'  # Possible site names
-        categories = '[ABCDS]'  # Possible category names
-        basename = os.path.basename(self.filename)
-        matches = re.search(r"^PF(%s)(%s)-?(\d{6})([brnm0-9])([0-9])\.fits" % (sites, categories), basename)
-        if not matches:
-            raise ValueError("Unable to interpret filename: %s" % (self.filename,))
-        site, category, visit, first, second = matches.groups()
-        visit = int(visit)
-        if category == "A":  # spectrograph
-            if first in "brnm":
-                arm = dict(b=1, r=2, n=3, m=4)[first]
-                spectrograph = int(second)
-            else:
-                spectrograph = int(first)
-                arm = int(second)
-            if spectrograph < 1 or spectrograph > 4:
-                raise RuntimeError(f"Unrecognised spectrograph ({spectrograph}) for base name {basename}")
-            return SimpleNamespace(site=site, category=category, visit=visit,
-                                   spectrograph=spectrograph, arm=arm)
-        elif category == "D":  # guider
-            sequence = 10*int(first) + int(second)
-            return SimpleNamespace(site=site, category=category, visit=visit, sequence=sequence)
+        return "INSTRUME" in header and header["INSTRUME"] == "PFS"
 
     @cache_translation
     def to_altaz_begin(self):
+        if "ALTITUDE" not in self._header or "AZIMUTH" not in self._header:
+            return AltAz(0.0*u.deg, 0.0*u.deg, obstime=self.to_datetime_begin(),
+                         location=self.to_location())
         return altaz_from_degree_headers(self, (("ALTITUDE", "AZIMUTH"),),
                                          self.to_datetime_begin())
 
@@ -123,7 +103,7 @@ class PfsTranslator(SubaruTranslator):
         # Neven writes:
         # > by comparing `DATE-OBS` for exposures with different `EXPTIME`, I am
         # > concluding that `DATE-OBS` refers to the end of the exposure
-        value = self.to_datetime_begin() - self._header["EXPTIME"]
+        value = self.to_datetime_end() - self._header["EXPTIME"]
         self._used_these_cards("EXPTIME")
         return value
 
@@ -134,67 +114,53 @@ class PfsTranslator(SubaruTranslator):
         return value
 
     @cache_translation
+    def to_detector_name(self):
+        arm = self._header["W_ARM"]
+        spectrograph = self._header["W_SPMOD"]
+        self._used_these_cards("W_ARM", "W_SPMOD")
+        return "brnm"[arm] + str(spectrograph)
+
+    @cache_translation
+    def to_physical_filter(self):
+        arm = self._header["W_ARM"]
+        self._used_these_cards("W_ARM")
+        return "brnm"[arm]
+
+    @cache_translation
     def to_detector_exposure_id(self):
         return 100*self.to_exposure_id() + self.to_detector_num()
 
     @cache_translation
     def to_detector_num(self):
-        return 10*self.to_detector_group() + self._armToIndex(self.to_detector_name())
-
-    @cache_translation
-    def to_detector_serial(self):
-        value = self._header["HIERARCH serial.CCD0"] + "+" + self._header["HIERARCH serial.CCD1"]
-        self._used_these_cards("HIERARCH serial.CCD0", "HIERARCH serial.CCD1")
-        return value
-
-    @cache_translation
-    def to_exposure_id(self):
-        return self.to_observation_id()
-
-    @cache_translation
-    def to_observation_id(self):
-        if "W_VISIT" in self._header:
-            self._used_these_cards("W_VISIT")
-            return self._header["W_VISIT"]
-        if self.filename is not None:
-            return self._parsedData.visit
-        raise ValueError("Can't determine visit number")
+        arm = self._header["W_ARM"]
+        spectrograph = self._header["W_SPMOD"]
+        self._used_these_cards("W_ARM", "W_SPMOD")
+        return 4*(spectrograph - 1) + arm - 1
 
     @cache_translation
     def to_tracking_radec(self):
         # Guess for what we'll use in the future
-        ra = self._header["RA2000"] if "RA2000" in self._header else np.nan
-        dec = self._header["DEC2000"] if "DEC2000" in self._header else np.nan
+        ra = self._header["RA2000"] if "RA2000" in self._header else 0.0
+        dec = self._header["DEC2000"] if "DEC2000" in self._header else 0.0
         radec = SkyCoord(ra, dec, frame="fk5", unit=(u.hourangle, u.deg),
                          obstime=self.to_datetime_begin(), location=self.to_location()).transform_to("icrs")
         self._used_these_cards("RA2000", "DEC2000")
         return radec
 
     @cache_translation
-    def to_visit_id(self):
-        return self.to_observation_id()
+    def to_instrument(self):
+        site = self._header["W_SITE"]
+        self._used_these_cards("W_SITE")
+        return "PFS" if site == "S" else f"PFS-{site}"
 
     @cache_translation
-    def to_instrument(self):
-        if self._parsedData is None:
-            self._parsedData = self.parseFilename(self.filename)
-        return "PFS" if self._parsedData.site == "S" else f"PFS-{self._parsedData.site}"
+    def to_dark_time(self):
+        if "DARKTIME" in self._header:
+            darkTime = self._header["DARKTIME"]
+            self._used_these_cards("DARKTIME")
+            return (darkTime, dict(unit=u.s))
+        return self.to_exposure_time()
 
-    def search_paths(self):
-        """Search paths to use when searching for header fix up correction
-        files.
-
-        This method is necessary for LSST 18.1.0. It can be removed once we
-        upgrade past ``w.2019.25``.
-
-        Returns
-        -------
-        paths : `list`
-            Directory paths to search. Can be an empty list if no special
-            directories are defined.
-
-        Notes
-        -----
-        Uses the classes ``default_search_path`` property if defined.
-        """
-        return [self.default_search_path]
+    @cache_translation
+    def to_ext_lamps(self):
+        return ",".join(sorted(getLamps(self._header)))
