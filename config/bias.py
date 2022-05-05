8eb9602c21cf66a5be2568d472c1b7455e6edbcf
Paul Price <price@astro.princeton.edu>
2022-05-05 11:12:13 -0400

    config: always use PfsIsrTask
    bias, dark and fiberFlat construction weren't configured to use PfsIsrTask
    but instead used the vanilla IsrTask from ip_isr. This wasn't a terrible
    problem, but for consistency going forward we should always use the
    PfsIsrTask.

--- config/bias.py
+++ config/bias.py
@@ -1,5 +1,8 @@
 import os.path
 from lsst.utils import getPackageDir
 
+from lsst.obs.pfs.isrTask import PfsIsrTask
+config.isr.retarget(PfsIsrTask)
+
 config.load(os.path.join(getPackageDir("obs_pfs"), "config", "calib.py"))
 config.isr.load(os.path.join(getPackageDir("obs_pfs"), "config", "isr.py"))
