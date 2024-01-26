05cc45d16064ffd78d6b58239e22b85f46534c8c
Paul Price <price@astro.princeton.edu>
2024-01-26 15:46:54 -0500

    isr: remove doFlatNir option
    Now we have fake flats for the optical arms, we don't need to
    treat the NIR any differently.

--- config/fiberFlat.py
+++ config/fiberFlat.py
@@ -8,8 +8,6 @@ config.load(os.path.join(getPackageDir("obs_pfs"), "config", "calib.py"))
 config.isr.load(os.path.join(getPackageDir("obs_pfs"), "config", "isr.py"))
 config.repair.load(os.path.join(getPackageDir("obs_pfs"), "config", "repair.py"))
 
-config.isr.doFlatNir = False  # We're building a flat
-
 # Don't extend into neighbour's space
 # This is specifically for flat creation, where the quartz spectra are narrowly spaced.
 config.profiles.profileRadius = 3
