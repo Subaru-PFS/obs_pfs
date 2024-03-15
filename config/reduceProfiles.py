1c9998a6f418f52c3e8b1d4807958b59e2b9dfe7
Paul Price <price@astro.princeton.edu>
2024-03-15 11:14:27 -1000

    config: adapt to reorganisation of reduceProfiles

--- config/reduceProfiles.py
+++ config/reduceProfiles.py
@@ -1,7 +1,6 @@
 import os.path
 from lsst.utils import getPackageDir
 
-from lsst.obs.pfs.isrTask import PfsIsrTask
-config.reduceExposure.isr.retarget(PfsIsrTask)
-
-config.reduceExposure.load(os.path.join(getPackageDir("obs_pfs"), "config", "reduceExposure.py"))
+reduceExposure = os.path.join(getPackageDir("obs_pfs"), "config", "reduceExposure.py")
+config.reduceExposure.load(reduceExposure)
+config.normalize.reduceExposure.load(reduceExposure)
