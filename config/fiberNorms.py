aef6cb89938811c620dfdcf01b5e76a7155c28b4
Paul Price <price@astro.princeton.edu>
2024-04-05 09:11:57 -0400

    add support for creating fiberNorms

---
+++ config/fiberNorms.py
@@ -0,0 +1,9 @@
+import os.path
+from lsst.utils import getPackageDir
+
+from lsst.obs.pfs.isrTask import PfsIsrTask
+config.isr.retarget(PfsIsrTask)
+
+config.load(os.path.join(getPackageDir("obs_pfs"), "config", "calib.py"))
+config.isr.load(os.path.join(getPackageDir("obs_pfs"), "config", "isr.py"))
+config.repair.load(os.path.join(getPackageDir("obs_pfs"), "config", "repair.py"))
