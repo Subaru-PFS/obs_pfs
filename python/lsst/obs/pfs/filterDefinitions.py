82c4699f909a2c59ee0482a6af91f031817d6da0
Paul Price <price@astro.princeton.edu>
2024-07-25 16:07:24 -0400

    filters: remove _PFS in the filter names.
    We don't really use these, so it doesn't really matter, but this means we
    don't have to worry about the difference between physical filter and label.

--- python/lsst/obs/pfs/filterDefinitions.py
+++ python/lsst/obs/pfs/filterDefinitions.py
@@ -4,8 +4,8 @@ __all__ = ("pfsFilterDefinitions",)
 
 
 pfsFilterDefinitions = FilterDefinitionCollection(
-    FilterDefinition("b_PFS", "b", "PFS blue arm"),
-    FilterDefinition("r_PFS", "r", "PFS red arm"),
-    FilterDefinition("m_PFS", "m", "PFS medium-resolution red arm"),
-    FilterDefinition("n_PFS", "n", "PFS near-infrared arm"),
+    FilterDefinition("b", "b", "PFS blue arm"),
+    FilterDefinition("r", "r", "PFS red arm"),
+    FilterDefinition("m", "m", "PFS medium-resolution red arm"),
+    FilterDefinition("n", "n", "PFS near-infrared arm"),
 )
