e5b3dc30692859b2e1440a72f6081af1798ee460
Paul Price <price@astro.princeton.edu>
2024-07-30 05:47:24 -1000

    remove bootstrap config overrides
    No longer required, with Gen3.

--- config/bootstrap.py
+++
@@ -1,6 +0,0 @@
-import os.path
-from lsst.utils import getPackageDir
-
-config.isr.load(os.path.join(getPackageDir("obs_pfs"), "config", "isr.py"))
-config.isr.doFlat = False
-config.findLines.threshold = 50.0
