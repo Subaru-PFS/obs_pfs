163a5f2ac9e5e41003094893ed573fed3fd215a8
Paul Price <price@astro.princeton.edu>
2025-02-11 09:59:47 -1000

    checkRawHeader: set NIR keywords
    Work adding support for up-the-ramp reads requires additional
    header keywords which aren't present in the simulated data.

--- bin.src/checkPfsRawHeaders.py
+++ bin.src/checkPfsRawHeaders.py
@@ -7,7 +7,7 @@ from lsst.obs.pfs.checkRawHeader import checkRawHeader
 from astropy import log
 
 
-def main(filenames: Iterable[str], allowFix: bool = False):
+def main(filenames: Iterable[str], allowFix: bool = False, doNIR: bool = False):
     """Main driver to check/fix PFS headers
 
     Parameters
@@ -16,13 +16,15 @@ def main(filenames: Iterable[str], allowFix: bool = False):
         List of files (or globs) for which to check/fix headers.
     allowFix : bool, optional
         Allow headers to be fixed?
+    doNIR : bool, optional
+        Check NIR files?
     """
     numGood = 0
     numBad = 0
     for gg in filenames:
         for fn in glob(gg):
             try:
-                checkRawHeader(fn, allowFix)
+                checkRawHeader(fn, allowFix, doNIR)
                 numGood += 1
             except Exception as exc:
                 log.warning(exc)
@@ -33,6 +35,7 @@ def main(filenames: Iterable[str], allowFix: bool = False):
 if __name__ == "__main__":
     parser = ArgumentParser(description="Check raw PFS files for header compliance")
     parser.add_argument("--fix", default=False, action="store_true", help="Fix bad/missing headers?")
+    parser.add_argument("--nir", default=False, action="store_true", help="Check NIR files?")
     parser.add_argument("filenames", nargs="+", help="Names of files (or glob) to check")
     args = parser.parse_args()
-    main(args.filenames, args.fix)
+    main(args.filenames, args.fix, args.nir)
