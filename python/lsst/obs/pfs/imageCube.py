d42305f50417fb87c0f919d2eea39b82cdf00f81
Craig Loomis <cloomis@astro.princeton.edu>
2025-02-27 07:50:27 -1000

    Make ImageCube a cache-on-read object with defined HDU names.
    - FITS read names are IMAGE_N with 1-based N. This is to match PFSB files
    - class methods use 0-based integers.
    - we write full cubes, and cache reads
    - save and make available the size of the cube as .nreads.

--- python/lsst/obs/pfs/imageCube.py
+++ python/lsst/obs/pfs/imageCube.py
@@ -22,9 +22,9 @@ class ImageCube:
 
     The images are stored in a FITS file, with each image in a separate HDU
     (in addition to a header-only primary HDU). The HDUs are named
-    ``"IMAGE_<index>"``, where ``<index>`` is an integer. There is no assumption
-    that the indices are contiguous, nor that they start at 0. There is no
-    enforcement of common dimensions or type for the images.
+    ``"IMAGE_<index>"``, where ``<index>`` is a 1-based integer. The image ids outside
+    the file are 0-based.
+    There is no enforcement of common dimensions or type for the images.
 
     Images are not read until they are requested, and are cached once read.
 
@@ -53,6 +53,17 @@ class ImageCube:
         self.metadata = metadata
         self._images: dict[int, ImageF] = {}
         self._closeOnDel = closeOnDel
+
+        if self.fits is not None:
+            try:
+                self.nreads = self.fits[0].header['NREADS']
+            except IndexError:
+                self.nreads = 0
+            except KeyError:
+                names = [hdu.name for hdu in self.fits if hdu.name.startswith("IMAGE_")]
+                self.nreads = max([self._getHduIndex(name) for name in names]) + 1
+        else:
+            self.nreads = 0
 
     def __enter__(self) -> "ImageCube":
         """Enter context"""
@@ -107,30 +118,61 @@ class ImageCube:
         fix_header(metadata, translator_class=PfsTranslator, filename=path)
         return cls(fits, metadata)
 
+    @classmethod
+    def fromCube(cls, data: np.ndarray, metadata: "PropertyList") -> "ImageCube":
+        """Construct ourselves from a data cube
+
+        Note: this does *not* copy the data
+
+        Parameters
+        ----------
+        data : `np.ndarray`
+            The data to load from.
+        metadata : `lsst.daf.base.PropertyList`
+            Metadata (FITS header) for the images.
+
+        Returns
+        -------
+        cube : `ImageCube`
+            The image cube.
+        """
+
+        self = cls.empty(metadata)
+        for i in range(len(data)):
+            self[i] = ImageF(data[i].astype(np.float32, copy=False))
+        self.nreads = len(data)
+        return self
+
     def flush(self) -> None:
         """Flush the cache"""
         self._images.clear()
+        self.nread = 0
 
     @classmethod
     def _getHduName(cls, index: int) -> str:
         """Return the name of the HDU for the image of interest
 
+        The HDU name is ``"IMAGE_<N>"``, where ``<N>`` is a 1-based integer.
+
         Parameters
         ----------
         index : `int`
-            The index of the image.
+            The index of the image. 0-based.
 
         Returns
         -------
         hduName : `str`
             The name of the HDU.
         """
-        return f"IMAGE_{index}"
+        return f"IMAGE_{index+1}"
 
     @classmethod
     def _getHduIndex(cls, hduName: str) -> int:
         """Return the index of the image of interest
 
+        The HDU name is ``"IMAGE_<N>"``, where ``<N>`` is a 1-based integer.
+        The read index is 0-based.
+
         Parameters
         ----------
         hduName : `str`
@@ -139,26 +181,35 @@ class ImageCube:
         Returns
         -------
         index : `int`
-            The index of the image.
+            The index of the image. 0-based
         """
-        return int(hduName.split("_")[-1])
+        return int(hduName.split("_")[-1])-1
 
     def __getitem__(self, index: int) -> ImageF:
         """Return the image for the given index"""
         if index in self._images:
             return self._images[index]
-        image = ImageF(self.fits[self._getHduName(index)].data.astype(np.float32))
+        image = ImageF(self.fits[self._getHduName(index)].data.astype(np.float32, copy=False))
         self._images[index] = image
         return image
 
     def __setitem__(self, index: int, image: ImageF) -> None:
         """Add the image for the given index"""
+        if not isinstance(index, int):
+            raise ValueError(f"Index must be an integer, not {index!r}")
         self._images[index] = image
+
+    def getNumReads(self) -> int:
+        """Return the number of images in the cube.
+
+        Sugar to match the method name in PfsRaw
+        """
+        return self.nreads
 
     def readAll(self) -> None:
         """Read all images into cache"""
         for hdu in self.fits[1:]:
-            self[self._getHduIndex(hdu.name)] = ImageF(hdu.data.astype(np.float32))
+            self[self._getHduIndex(hdu.name)] = ImageF(hdu.data.astype(np.float32, copy=False))
 
     def writeFits(self, path: str) -> None:
         """Write the images
@@ -172,11 +223,37 @@ class ImageCube:
         """
         fits = astropy.io.fits.HDUList()
 
-        fits.append(astropy.io.fits.PrimaryHDU(header=astropyHeaderFromDict(self.metadata)))
+        header = astropyHeaderFromDict(self.metadata)
+        header['NREADS'] = self.nreads
+        fits.append(astropy.io.fits.PrimaryHDU(data=None, header=header))
         for index in sorted(self._images):
-            fits.append(astropy.io.fits.ImageHDU(self._images[index].array, name=self._getHduName(index)))
+            fits.append(astropy.io.fits.CompImageHDU(self._images[index].array, name=self._getHduName(index)))
         with open(path, "wb") as fd:
             fits.writeto(fd)
+
+    @classmethod
+    def writeCubeData(cls, path: str, data: np.ndarray, metadata: "PropertyList") -> None:
+        """Directly write the data to a FITS file
+
+        Parameters
+        ----------
+        path : `str`
+            Path to the output FITS file.
+        data : `np.ndarray`
+            The data to write.
+        metadata : `lsst.daf.base.PropertyList`
+            Metadata (FITS header) for the images.
+        """
+
+        with astropy.io.fits.open(path, mode='append') as fits:
+            header = astropyHeaderFromDict(metadata)
+            header['NREADS'] = self.nreads
+            phdu = astropy.io.fits.PrimaryHDU(data=None, header=header)
+            fits.append(phdu)
+            for i in range(len(data)):
+                imHdu = astropy.io.fits.CompImageHDU(data[i], name=cls._getHduName(i))
+                fits.append(imHdu)
+                fits.flush()
 
     @classmethod
     def readFits(cls, path: str) -> "ImageCube":
