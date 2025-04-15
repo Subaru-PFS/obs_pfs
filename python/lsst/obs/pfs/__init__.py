1e6484d4321f895fa981e9467a48cb61a1411b1c
Paul Price <price@astro.princeton.edu>
2025-04-15 10:58:48 -1000

    set __version__
    so that the package version can be recorded by the LSST framework.

--- python/lsst/obs/pfs/__init__.py
+++ python/lsst/obs/pfs/__init__.py
@@ -1,1 +1,7 @@
+try:
+    from .version import *
+except ImportError:
+    print("WARNING: unable to import version.py in obs_pfs; did you build with scons?")
+    __version__ = "unknown"
+
 from .PrimeFocusSpectrograph import *
